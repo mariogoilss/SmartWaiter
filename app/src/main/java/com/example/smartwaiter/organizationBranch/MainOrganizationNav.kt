1f734e33500185695f14d1dc2b2fde06552c3f32
mariogmez <mariogomex199921@gmail.com>
2022-11-23 20:38:25 +0100

    CMT 23/11 20:40

--- app/src/main/java/com/example/smartwaiter/organizationBranch/MainOrganizationNav.kt
+++ app/src/main/java/com/example/smartwaiter/organizationBranch/MainOrganizationNav.kt
@@ -27,7 +27,7 @@ class MainOrganizationNav : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_menu, R.id.navigation_management
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/example/smartwaiter/organizationBranch/ui/management/ManagementFragment.kt
@@ -0,0 +1,50 @@
+package com.example.smartwaiter.organizationBranch.ui.management
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.findNavController
+import com.example.smartwaiter.MainActivity
+import com.example.smartwaiter.Prefs.PreLoad.Companion.prefs
+import com.example.smartwaiter.R
+import com.example.smartwaiter.databinding.FragmentManagementBinding
+import com.example.smartwaiter.databinding.FragmentMenuBinding
+import com.google.android.material.tabs.TabLayout
+
+class ManagementFragment : Fragment() {
+
+    private var _binding: FragmentManagementBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentManagementBinding.inflate(inflater, container, false)
+        val bt:Button = binding.btnMngCloseSession
+        val image:ImageView = binding.imgPrueba
+        image.setImageResource(R.drawable.grafica);
+        val root: View = binding.root
+
+        bt.setOnClickListener {
+            prefs.wipe()
+            val intent = Intent(context, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
