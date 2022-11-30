326c0dddaae4f3d1d6e209711d85c95399eb5c5a
mariogmez <mariogomex199921@gmail.com>
2022-11-30 19:18:24 +0100

    CMT 30/11 19:18

---
+++ app/src/main/java/com/example/smartwaiter/clientBranch/MainClientActivityNav.kt
@@ -0,0 +1,36 @@
+package com.example.smartwaiter.clientBranch
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.smartwaiter.R
+import com.example.smartwaiter.databinding.ActivityMainClientNavBinding
+
+class MainClientActivityNav : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainClientNavBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainClientNavBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main_client)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
